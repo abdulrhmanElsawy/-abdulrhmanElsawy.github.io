00e1c79856e0149ee945326090bc6d7a53cd7993
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2021-11-19 11:18:57 +0200

    Add files via upload

--- js/front.js
+++ js/front.js
@@ -9,12 +9,16 @@ var $secondLeftimg = document.getElementById("second-left-img");
 var $secondRightPara = document.getElementById("first-right-para");
 var $thirdLeftPara = document.getElementById("third-left-para");
 var $thirdRightimg = document.getElementById("third-right-img");
+var $navbar = document.getElementById("navbar");
 
 $window.on('scroll', function() {
 
     $myDist = $window.scrollTop();
     if ($myDist > ($aboutdiv.offsetTop - 200)) {
         $("#about-para").css("opacity", "1");
+        $(".navbar").css({
+            backgroundColor: "#111"
+        });
     }
 
     if ($myDist > ($firstLeftPara.offsetTop + 900)) {
@@ -33,6 +37,21 @@ $window.on('scroll', function() {
         $("#third-left-para").css("left", "0");
         $("#third-right-img").css("left", "0");
 
+    }
+
+    if ($myDist > ($navbar.offsetTop)) {
+        $(".navbar").css({
+            position: "fixed",
+            top: 0
+        });
+    }
+
+    if (($myDist - 100) < ($navbar.offsetTop)) {
+        $(".navbar").css({
+            position: 'absolute',
+            top: 'inherit',
+            backgroundColor: '#ffffff10'
+        });
     }
 
 
--- js/tourguideregpage.js
+++ js/tourguideregpage.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     var form1 = document.getElementById("form1"),
         form2 = document.getElementById("form2"),
         form3 = document.getElementById("form3"),
+        form4 = document.getElementById("form4"),
+        form5 = document.getElementById("form5"),
         next1 = document.getElementById("next1"),
         next2 = document.getElementById("next2"),
         next3 = document.getElementById("next3"),
@@ -9,11 +11,17 @@ $(document).ready(function() {
         back1 = document.getElementById("back1"),
         back2 = document.getElementById("back2"),
         back3 = document.getElementById("back3"),
+        submitAllForms = document.getElementById("submit-all-forms"),
         back4 = document.getElementById("back4"),
         Email = document.getElementById("Email"),
         password = document.getElementById("password"),
         confirmPassword = document.getElementById("confirm-password"),
         progress = document.getElementById("progress");
+
+
+    submitAllForms.onclick = function() {
+        $('#form1 #form2 #form3 #form4').submit();
+    };
 
 
     next1.onclick = function() {
@@ -131,16 +139,57 @@ $(document).ready(function() {
 
 
 
-    $("[type=file]").on("change", function(){
+    $("[type=file]").on("change", function() {
         // Name of file and placeholder
         var file = this.files[0].name;
         var dflt = $(this).attr("placeholder");
-        if($(this).val()!=""){
-          $(this).next().text(file);
+        if ($(this).val() != "") {
+            $(this).next().text(file);
         } else {
-          $(this).next().text(dflt);
+            $(this).next().text(dflt);
         }
-      });
+    });
+
+
+    form1.onkeyup = function() {
+        if (!$("#Fname").val() || !$("#Lname").val() || !$("#usernmae").val() || !$("#region").val() || !$("#country").val() || !$("#pnonenumber").val() || !$("#confirm-password").val() || !$("#password").val() || !$("#Email").val()) {
+            $("#next1").css("pointerEvents", "none")
+        } else {
+            $("#next1").css("pointerEvents", "auto")
+        }
+    };
+
+    form2.onkeyup = function() {
+        if (!$("#jobtitle").val()) {
+            $("#next2").css("pointerEvents", "none")
+        } else {
+            $("#next2").css("pointerEvents", "auto")
+        }
+    };
+
+    form3.onkeyup = function() {
+        if (!$("#degree-one").val() || !$("#university-one").val() || !$("#date-one").val()) {
+            $("#next3").css("pointerEvents", "none")
+        } else {
+            $("#next3").css("pointerEvents", "auto")
+        }
+    };
+
+    form4.onkeyup = function() {
+        if ($("#lang1").val() == "" || $("#Speaking1").val() == "" || $("#Writting1").val() == "" || $("#Comprehension1").val() == "") {
+            $("#next4").css("pointerEvents", "none")
+        } else {
+            $("#next4").css("pointerEvents", "auto")
+        }
+    };
+
+    form5.onmouseover = function() {
+        if (!$("#f02").val() || !$("#f03").val() || !$("#f04").val()) {
+            $("#submit-all-forms").css("pointerEvents", "none")
+        } else {
+            $("#submit-all-forms").css("pointerEvents", "auto")
+        }
+    };
 
 
 });
