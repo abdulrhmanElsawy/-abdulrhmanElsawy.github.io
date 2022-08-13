665375ec3f125b04ec8474b82571cfdcf54caf26
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-08-13 13:32:33 +0200

    Add files via upload

---
+++ maktab/js/front.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+    $(".open-nav").click(function() {
+        $(".mobile-nav").slideDown(400);
+    })
+
+    $(".close-nav").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+    $(".mobile-nav ul li").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+
+});
