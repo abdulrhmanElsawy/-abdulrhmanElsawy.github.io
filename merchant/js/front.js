02127b83706cac0b315a645e5c907e16a98ebf21
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-10-29 23:39:35 +0200

    Add files via upload

---
+++ merchant/js/front.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    
+    $('.landing-slider .slider').slick({
+        dots: false,
+        arrows:false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay:true,
+        autoplayspeed:3000,
+        cssEase: 'linear'
+    });
+
+
+});
