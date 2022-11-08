e3201f9297a5789c3e8859d8055aa24efafd5eb2
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-11-08 18:51:36 +0200

    Add files via upload

---
+++ Skafo/js/front.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+
+    $(".open-nav").click(function() {
+        $(".mobile-nav").slideDown(400);
+    })
+
+    $(".close-nav").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+    $(".mobile-nav ul li").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+
+    
+    $('.landing-slider .slider').slick({
+        dots: false,
+        arrows:false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay:true,
+        autoplayspeed:3000,
+    });
+
+
+    $('.rates-slider .slider').slick({
+        dots: false,
+        arrows:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false,
+              arrows:false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+                      
+
+});
