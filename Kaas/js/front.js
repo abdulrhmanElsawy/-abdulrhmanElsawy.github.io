499636aad6b52c429c0ad0d645944ae3541bcf8f
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-11-11 19:57:13 +0200

    Add files via upload

--- Kaas/js/front.js
+++ Kaas/js/front.js
@@ -11,6 +11,14 @@ $(document).ready(function() {
 
     })
 
+    $(".social-pop img").click(function(){
+        $(".pop-up.max-min").fadeIn(400);
+    })
+
+
+    $(".close-max-min-popup").click(function(){
+        $(".pop-up.max-min").fadeOut(400);
+    })
 
 
     $(".doowr16-content>div:not(.lines)").click(function(){
@@ -228,11 +236,84 @@ $(document).ready(function() {
             var img = `<img crossorigin="anonymous" src="${thisImgSrc}">`;
             var text = `<h1>${thisText}</h1>`;
 
+
+          
+            
+            var element = `
+            <li>
+                <img crossorigin="anonymous" src="${thisImgSrc}">
+                ${thisText}
+            </li>
+        `;
+
+
             $(`#${divId}`).empty();
 
             $(`#${divId}`).html(img);
 
             $(this).addClass("active");
+
+
+            $(".doowr1 .pop-up ul").append(element);
+            var element2 = `
+            <img src="${thisImgSrc}">
+            `;
+            $(`#spot_${divId}`).empty();
+            console.log(`#spot_${divId}`);
+
+            $(`#spot_${divId}`).append(element2);
+
+            $(".pop-up").fadeOut(400);
+
+            divId = 0;
+
+            }
+        });
+
+    
+
+        
+    });
+
+
+
+
+
+    $(".doowr1-content>div:not(.lines)").click(function(){
+        var divId = $(this).attr('id');
+
+
+        $(".doowr1 .pop-up").fadeIn(400);
+
+        let counter = 0;
+
+
+
+
+
+
+        $(document).on('click', '.doowr1 .pop-up ul li', function() {
+
+            
+            if(counter == 0){
+
+                counter = 1;
+
+
+            var thisImgSrc = $(this).children("img").attr("src");
+            var thisText = $(this).text();
+            var img = `<img crossorigin="anonymous" src="${thisImgSrc}">`;
+            var text = `<h1>${thisText}</h1>`;
+
+
+            $(`#${divId}`).empty();
+
+            $(`#${divId}`).html(img);
+
+            $(this).addClass("active");
+
+            
+
 
             var element2 = `
             <img src="${thisImgSrc}">
@@ -253,6 +334,10 @@ $(document).ready(function() {
 
         
     });
+
+
+
+    
 
     
     // Define the function 
