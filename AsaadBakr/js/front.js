e9b1de0695258549e0c8d41cd49e25c9b4c0a1c5
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-11-21 17:52:13 +0200

    Add files via upload

---
+++ AsaadBakr/js/front.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+
+
+    $('.partners-slider .slider').slick({
+        dots: false,
+        arrows:false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        // autoplay:true,
+        // autoplayspeed:2000,
+        variableWidth: true
+    });
+
+    $('.rates-slider .slider').slick({
+        dots: false,
+        arrows:false,
+        // autoplay:true,
+        // autoplayspeed:2000,
+    });
+    
+
+
+
+    $(".open-nav").click(function() {
+        $(".mobile-nav").slideDown(400);
+    })
+
+    $(".close-nav").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+    $(".mobile-nav ul li").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+    $(".questions .left .question h1").click(function(){
+        $(this).toggleClass("active");
+        $(this).next().slideToggle("300");
+    })
+
+
+
+});
