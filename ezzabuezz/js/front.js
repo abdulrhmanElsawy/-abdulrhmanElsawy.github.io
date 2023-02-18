bbe835eb7971e1ca2991f2c7edbbcc65f587af4e
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2023-02-18 21:25:57 +0200

    Add files via upload

---
+++ ezzabuezz/js/front.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+
+
+    $(".open-nav").click(function() {
+        $(".mobile-nav").slideDown(400);
+    })
+
+    $(".close-nav").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+    $(".mobile-nav ul li").click(function() {
+        $(".mobile-nav").slideUp(400);
+    })
+
+
+
+});
+
